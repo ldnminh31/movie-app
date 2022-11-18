9ec2443717a6cdf540dcdb1ac91189c698f089d2
Minh Le <ledoannhatminh31@gmail.com>
2022-11-18 15:48:11 +0700

    setup server

--- node/src/app.js
+++ node/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const movieController = require("./controllers/movie.controller");
-const ApiError = require('./api-error');
+const ApiError = require("./api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -11,26 +11,54 @@ app.get("/", (req, res) => {
     message: "Welcome to MovieApp",
   });
 });
-app
-  .route("/api/movie")
-  .get(movieController.findAll)
-  .post(movieController.create)
-  .delete(movieController.deleteAll);
+// user api
+app.route("/api/auth/signin").post(movieController.signIn);
+
+app.route("/api/auth/signup").post(movieController.signUp);
+
+app.route("/api/auth/signout").post(movieController.signOut);
+
+// movie api
+app.route("/api/movie").get(movieController.all).post(movieController.addFilm);
 
 app
-  .route("/api/movie/:id(\\d+)")
-  .get(movieController.findOne)
-  .put(movieController.update)
-  .delete(movieController.delete);
+  .route("/api/movie/:id")
+  .get(movieController.findFilm)
+  .put(movieController.updateFilm)
+  .delete(movieController.deleteFilm);
+
+// category api
+app
+  .route("/api/category")
+  .get(movieController.category)
+  .post(movieController.addCat);
+
+app
+  .route("/api/category/:id")
+  .get(movieController.findCat) //not find Cat :)
+  .put(movieController.updateCat)
+  .delete(movieController.deleteCat);
+
+// comment api
+app
+  .route("/api/comment")
+  .get(movieController.comment)
+  .post(movieController.addCom);
+
+app
+  .route("/api/comment/:id")
+  .put(movieController.updateCom)
+  .delete(movieController.deleteCom);
 
 //   handle 404 respone
-app.use((req, res, next)=> {
-    return next(new ApiError(404, 'Resource not found'));
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
 });
-app.use((error,req,res,next)=>{
-    return res.status(error.statusCode || 500).json({
-        message: error.message || 'Internal Server Error',
-    });
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
--- node/src/controllers/movie.controller.js
+++ node/src/controllers/movie.controller.js
@@ -1,18 +1,58 @@
-exports.create = (req, res) => {
-    return res.send({message: 'create handler'});
+// User controller
+exports.signIn = (req, res) => {
+  return res.send({ message: "signIn handler" });
 };
-exports.findAll = (req, res) => {
-    return res.send({message: 'findAll handler'});
+exports.signUp = (req, res) => {
+  return res.send({ message: "signUp handler" });
 };
-exports.findOne = (req, res) => {
-    return res.send({message: 'findOne handler'});
+exports.signOut = (req, res) => {
+  return res.send({ message: "signOut handler" });
 };
-exports.update = (req, res) => {
-    return res.send({message: 'update handler huhu'});
+
+// Movie controller
+exports.all = (req, res) => {
+  return res.send({ message: "Retrieve all movie handler" });
 };
-exports.delete = (req, res) => {
-    return res.send({message: 'delete handler'});
+exports.findFilm = (req, res) => {
+  return res.send({ message: "Search Film handler" });
 };
-exports.deleteAll = (req, res) => {
-    return res.send({message: 'deleteAll handler'});
+exports.addFilm = (req, res) => {
+  return res.send({ message: "add film handler" });
+};
+exports.updateFilm = (req, res) => {
+  return res.send({ message: "update film handler" });
+};
+exports.deleteFilm = (req, res) => {
+  return res.send({ message: "delete film handler" });
+};
+
+// Category controller
+exports.category = (req, res) => {
+  return res.send({ message: "Retrieve all category handler" });
+};
+exports.findCat = (req, res) => {
+  return res.send({ message: "Search category handler" });
+};
+exports.addCat = (req, res) => {
+  return res.send({ message: "add cat handler" });
+};
+exports.updateCat = (req, res) => {
+  return res.send({ message: "update cat handler" });
+};
+exports.deleteCat = (req, res) => {
+  return res.send({ message: "delete cat handler" });
+};
+
+// Comment controller
+exports.comment = (req, res) => {
+  return res.send({ message: "retrive all cmt handler" });
+};
+exports.addCom = (req, res) => {
+  return res.send({ message: "add cmt handler" });
+};
+exports.updateCom = (req, res) => {
+  return res.send({ message: "update cmt handler" });
+};
+exports.deleteCom = (req, res) => {
+  return res.send({ message: "delete cmt handler" });
 };
